6fba4e46db574a08caca51540cf9110aef87beaa
Joseph Nielsen <josephmnielsen@gmail.com>
2021-03-09 09:52:02 -0800

    initial push

---
+++ Develop/app.js
@@ -0,0 +1,119 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const { prompt } = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const role = () => {
+
+  prompt ({
+    type: 'list',
+    name: 'role',
+    message: 'What is the role of the employee?',
+    choices: ['Manager', 'Engineer', 'Intern']
+  })
+  
+}
+
+const eRole = () => {
+  prompt ({
+    type: 'input',
+    name: 'name',
+    message: 'Enter the employee name:'
+  }, {
+    type: 'input',
+    name: 'id',
+    message: 'Enter employee id:'
+  }, {
+    type: 'input',
+    name: 'email',
+    message: 'Enter the employee email:'
+  }, {
+    type: 'input',
+    name: 'github',
+    message: 'Enter employee github username:'
+  }, {
+    type: 'confirm',
+    name: 'another',
+    message: 'Would you like to enter another employee?'
+  })
+}
+
+const iRole = () => {
+  prompt({
+    type: 'input',
+    name: 'name',
+    message: 'Enter the employee name:'
+  }, {
+    type: 'input',
+    name: 'id',
+    message: 'Enter employee id:'
+  }, {
+    type: 'input',
+    name: 'email',
+    message: 'Enter the employee email:'
+  }, {
+    type: 'input',
+    name: 'school',
+    message: 'Enter employee school:'
+  }, {
+    type: 'confirm',
+    name: 'another',
+    message: 'Would you like to enter another employee?'
+  })
+}
+
+const mRole = () => {
+  prompt({
+    type: 'input',
+    name: 'name',
+    message: 'Enter the employee name:'
+  }, {
+    type: 'input',
+    name: 'id',
+    message: 'Enter employee id:'
+  }, {
+    type: 'input',
+    name: 'email',
+    message: 'Enter the employee email:'
+  }, {
+    type: 'input',
+    name: 'office',
+    message: 'Enter employee office number:'
+  }, {
+    type: 'confirm',
+    name: 'another',
+    message: 'Would you like to enter another employee?'
+  })
+}
+
+
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
